0fb71e9b9126f37300d8cd2b0b6144a8666c6002
unknown <liumoyan@gmail.com>
2021-12-08 20:15:41 +0800

    add print hi

--- perpetual_arbitrag/src/main.py
+++ perpetual_arbitrag/src/main.py
@@ -6,6 +6,7 @@ from clients.KucoinApiClient import KucoinApiClient
 from execution.BotExecution import BotExecution 
 from strategies.SingleTradeArbitrag import SingleTradeArbitrag, ExecutionDecision
 
+print("hi")
 """
 python3 main.py \
 --spot_trading_pair BTC-USDT \
