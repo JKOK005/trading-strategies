293d2c0e152c3215f3cb181e3bf8348d6ead5fe5
Johan Kok <johan.kok@grabtaxi.com>
2022-01-26 16:45:58 +0800

    Starting implementation of OkexApiClient class

---
+++ perpetual_arbitrag/src/clients/OkexApiClient.py
@@ -0,0 +1,128 @@
+import datetime
+import logging
+import sys
+from okex.futures_api import FutureApi
+from okex.spot_api import SpotApi
+from clients.ExchangeClients import ExchangeClients
+
+class OkexApiClient(ExchangeClients):
+	def __init__(self, 	api_key: str, 
+						api_secret_key: str,
+						passphrase: str):
+		pass
+
+	def get_spot_trading_account_details(self, currency: str):
+		"""
+		Retrieves spot trading details
+		"""
+		pass
+
+	def get_futures_trading_account_details(self, currency: str):
+		"""
+		Retrieves futures trading account details
+		"""
+		pass
+
+	def get_spot_trading_price(self, symbol: str):
+		"""
+		Retrieves current spot pricing for trading symbol
+		"""
+		pass
+
+	def get_futures_trading_price(self, symbol: str):
+		"""
+		Retrieves current futures price for trading symbol
+		"""
+		pass
+
+	def get_spot_min_volume(self, symbol: str):
+		"""
+		Retrieves minimum order volume for spot trading symbol
+		"""
+		pass
+
+	def get_futures_min_lot_size(self, symbol: str):
+		"""
+		Retrieves minimum order lot size for futures trading symbol
+		"""
+		pass
+
+	def get_spot_average_bid_ask_price(self, symbol: str, size: float):
+		"""
+		Returns the average bid / ask price of the spot asset, assuming that we intend to trade at a given volume. 
+		"""
+		pass
+
+	def get_futures_average_bid_ask_price(self, symbol: str, size: float):
+		"""
+		Returns the average bid / ask price of the futures asset, assuming that we intend to trade at a given lot size. 
+		"""
+		pass
+
+	def get_spot_open_orders(self, symbol: str):
+		"""
+		Gets information of all open spot orders by the user
+		"""
+		pass
+
+	def get_futures_open_orders(self, symbol: str):
+		"""
+		Gets information of all open future orders by the user
+		"""
+		pass
+
+	def get_spot_most_recent_open_order(self, symbol: str):
+		"""
+		Gets the most recent open orders for spot
+		"""
+		pass
+
+	def get_futures_most_recent_open_order(self, symbol: str):
+		"""
+		Gets the most recent open orders for futures
+		"""
+		pass
+
+	def get_spot_fulfilled_orders(self, symbol: str):
+		"""
+		Gets information for all fulfilled spot orders by the user
+		"""
+		pass
+
+	def get_futures_fulfilled_orders(self, symbol: str):
+		"""
+		Gets information of all fulfilled future orders by the user
+		"""
+		pass
+
+	def get_spot_most_recent_fulfilled_order(self, symbol: str):
+		"""
+		Gets information of the most recent spot trade that have been fulfilled
+		"""
+		pass
+
+	def get_futures_most_recent_fulfilled_order(self, symbol: str):
+		"""
+		Gets information of the most recent futures trade that have been fulfilled
+		"""
+		pass
+
+	def get_futures_effective_funding_rate(self, symbol: str, seconds_before: int):
+		"""
+		Gets the effective funding rate for futures contract.
+
+		Effective funding rate takes into account a variety of factors to decide on the funding rate.
+		"""
+		pass
+ 
+	def place_spot_order(self, *args, **kwargs):
+		pass
+
+	def place_futures_order(self, *args, **kwargs):
+		pass
+
+	def cancel_spot_order(self, order_id: str):
+		pass
+
+	def cancel_futures_order(self, order_id: str):
+		pass
