405a89ec1c90735aaa6fb0ae5e9bb92fca8d8e40
johan <JKOK005@e.ntu.edu.sg>
2022-01-27 12:44:54 +0800

    Continue development of OkexApiClient

--- perpetual_arbitrag/src/clients/OkexApiClient.py
+++ perpetual_arbitrag/src/clients/OkexApiClient.py
@@ -60,20 +60,75 @@ class OkexApiClient(ExchangeSpotClients, ExchangePerpetualClients):
 	def get_perpetual_min_lot_size(self, symbol: str):
 		"""
 		Retrieves minimum order lot size for perpetual trading symbol
+
+		# Based on definition in https://www.okx.com/docs/en/#swap-swap---orders, minimum contract size is 1
 		"""
-		pass
+		return 1
+
+	def _compute_average_margin_purchase_price(self, price_qty_pairs_ordered: [float, float], size: float):
+		"""
+		We will read pricing - qty data from the first entry of the list. 
+
+		This logic will differ, depending on whether we want to go long / short on the asset. 
+		As such, the ordering of the price-qty pairs in the list has to be handled properly by the user. 
+		"""
+		all_trade_qty 		= size
+		trade_amt 			= 0
+		executed_trade_qty 	= 0
+		for each_price_qty_pairs in price_qty_pairs_ordered:
+			if all_trade_qty < 0:
+				break
+			else:
+				[price, qty] 	=  each_price_qty_pairs
+				trade_qty 		=  min(all_trade_qty, qty)
+				trade_amt 	 	+= price * trade_qty
+				all_trade_qty 	-= trade_qty
+				executed_trade_qty += trade_qty
+		return trade_amt / executed_trade_qty
+
+	def _compute_average_bid_price(self, bids: [[float, float]], size: float):
+		# Sell into bids starting from the highest to the lowest.
+		_bids 	= sorted(bids, key = lambda x: x[0], reverse = True)
+		if len(_bids) > 0:
+			return self._compute_average_margin_purchase_price(price_qty_pairs_ordered = _bids, size = size)
+		return 0
+
+	def _compute_average_ask_price(self, asks: [[float, float]], size: float):
+		# Buy into asks starting from the lowest to the highest.
+		_asks 	= sorted(asks, key = lambda x: x[0], reverse = False)
+		if len(_asks) > 0:
+			return self._compute_average_margin_purchase_price(price_qty_pairs_ordered = _asks, size = size)
+		return sys.maxsize
 
 	def get_spot_average_bid_ask_price(self, symbol: str, size: float):
 		"""
-		Returns the average bid / ask price of the spot asset, assuming that we intend to trade at a given volume. 
+		Returns the average bid / ask price of the spot asset, assuming that we intend to trade at a given volume.
+
+		# TODO: Assert that number of liquidated orders in returned bid_ask_orders list is not needed in computation
 		"""
-		pass
+		bid_ask_orders 		= self.spot_client.get_depth(instrument_id = symbol, size = 100)
+		bids 				= bid_ask_orders["bids"]
+		bids 				= list(map(lambda x: [float(x[0]), float(x[1])], bids))
+		average_bid_price 	= self._compute_average_bid_price(bids = bids, size = size)
+
+		asks 				= bid_ask_orders["asks"]
+		asks 				= list(map(lambda x: [float(x[0]), float(x[1])], asks))
+		average_sell_price 	= self._compute_average_ask_price(asks = asks, size = size)
+		return (average_bid_price, average_sell_price)
 
 	def get_perpetual_average_bid_ask_price(self, symbol: str, size: float):
 		"""
 		Returns the average bid / ask price of the perpetual asset, assuming that we intend to trade at a given lot size. 
 		"""
-		pass
+		bid_ask_orders 		= self.perp_client.get_depth(instrument_id = symbol, size = 100)
+		bids 				= bid_ask_orders["bids"]
+		bids 				= list(map(lambda x: [float(x[0]), float(x[1])], bids))
+		average_bid_price 	= self._compute_average_bid_price(bids = bids, size = size)
+
+		asks 				= bid_ask_orders["asks"]
+		asks 				= list(map(lambda x: [float(x[0]), float(x[1])], asks))
+		average_sell_price 	= self._compute_average_ask_priclientce(asks = asks, size = size)
+		return (average_bid_price, average_sell_price)
 
 	def get_spot_open_orders(self, symbol: str):
 		"""
