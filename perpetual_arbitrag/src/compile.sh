2c14e5dde7c6f489e604c30847729aad0e4f8573
johan <JKOK005@e.ntu.edu.sg>
2022-02-16 23:52:59 +0800

    Compile.sh script

---
+++ perpetual_arbitrag/src/compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+version='latest'
+
+echo $version
+echo "Compiling Kucoin bot"
+docker build -t jkok005/kucoin-spot-perp-arb:$version -f ./main/intra_exchange/kucoin/spot_perp/Dockerfile .
+
+echo "Compiling Okx bot"
+docker build -t jkok005/okx-spot-perp-arb:$version -f ./main/intra_exchange/okx/spot_perp/Dockerfile .
