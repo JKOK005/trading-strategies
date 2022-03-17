caf190ab514c3f27af04e5091188c9477fb0e31d
johan <JKOK005@e.ntu.edu.sg>
2022-03-17 12:09:16 +0800

    Moved latency checking decorator to out of class
    Implement lazy connection by creating a new connect function

--- perpetual_arbitrag/src/feeds/CryptoStoreRedisFeeds.py
+++ perpetual_arbitrag/src/feeds/CryptoStoreRedisFeeds.py
@@ -1,7 +1,22 @@
 import json
+import logging
 import redis
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 from feeds.PriceFeeds import PriceFeeds
+from functools import wraps
+
+def assert_latency(func):
+	"""
+	Asserts if response is within latency ms from current time.
+	response needs an `updated` field denoting the latest time of the information.
+	"""
+	@wraps(func)
+	def wrapper(*args, **kwargs):
+		resp 	= func(*args, **kwargs)
+		resp_df = datetime.utcfromtimestamp(resp["updated"])
+		assert datetime.utcnow() - resp_df <= timedelta(milliseconds = args[0].permissible_latency_s * 1000), "latency too large"
+		return resp
+	return wrapper
 
 class CryptoStoreRedisFeeds(PriceFeeds):
 	"""
@@ -10,6 +25,7 @@ class CryptoStoreRedisFeeds(PriceFeeds):
 	"""
 	redis_cli = None
 	permissible_latency_s = 0
+	logger = logging.getLogger('CryptoStoreRedisFeeds')
 
 	def __init__(self, 	redis_url: str, 
 						redis_port: int,
@@ -17,9 +33,15 @@ class CryptoStoreRedisFeeds(PriceFeeds):
 						*args, **kwargs):
 
 		super(CryptoStoreRedisFeeds, self).__init__(*args, **kwargs)
-		self.redis_cli = redis.Redis(host = redis_url, port = redis_port)
+		self.redis_url = redis_url
+		self.redis_port = redis_port
 		self.permissible_latency_s = permissible_latency_s
 		return
+
+	def connect(self):
+		self.logger.debug(f"Connecting to redis at {self.redis_url}:{self.redis_port}")
+		self.redis_cli = redis.Redis(host = self.redis_url, port = self.redis_port)
+		return self
 
 	def symbol_to_key_mapping(self, symbol: str, exchange: str):
 		new_symbol = symbol
@@ -27,22 +49,9 @@ class CryptoStoreRedisFeeds(PriceFeeds):
 			# OKX symbols are denoted by SWAP but crypto store uses PERP
 			new_symbol = new_symbol.replace("SWAP", "PERP").replace("swap", "PERP")
 		return new_symbol
-
-	def assert_latency(func):
-		"""
-		Asserts if response is within latency ms from current time.
-		response needs an `updated` field denoting the latest time of the information.
-		"""
-		def wrapper(*args, **kwargs):
-			resp 	= func(*args, **kwargs)
-			resp_df = datetime.utcfromtimestamp(resp["updated"])
-			assert datetime.utcnow() - resp_df <= timedelta(milliseconds = args[0].permissible_latency_s * 1000), "latency too large"
-			return resp
-		return wrapper
 			
 	@assert_latency
 	def sorted_order_book(self, symbol: str, exchange: str, *args, **kwargs):
-		start 		= datetime.utcnow()
 		new_symbol 	= self.symbol_to_key_mapping(symbol = symbol, exchange = exchange)
 		redis_key 	= f"book-{exchange}-{new_symbol}" 		# Exchange and symbols are all UPPER case
 		resp 		= self.redis_cli.zrange(redis_key, -1, -1)[0]
