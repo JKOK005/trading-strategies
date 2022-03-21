e0dc900abb96d450480f4b3cdc12fa22b3b7ca98
johan <JKOK005@e.ntu.edu.sg>
2022-03-21 10:15:28 +0800

    Kucoin Api Client WS class implementation

---
+++ perpetual_arbitrag/src/clients/KucoinApiClientWS.py
@@ -0,0 +1,39 @@
+import asyncio
+import base64
+import json
+import hashlib
+import hmac
+import logging
+import websockets
+from datetime import datetime
+from clients.KucoinApiClient import KucoinApiClient
+
+class KucoinApiClientWS(OkxApiClient):
+	logger 				= logging.getLogger('KucoinApiClientWS')
+
+	def __init__(self, feed_client, *args, **kwargs):
+		super(KucoinApiClientWS, self).__init__(*args, **kwargs)
+		self.feed_client = feed_client
+		return
+
+	def get_spot_average_bid_ask_price(self, symbol: str, size: float):
+		"""
+		Returns the average bid / ask price of the spot asset, assuming that we intend to trade at a given volume. 
+		"""
+		order_book 			= self.feed_client.sorted_order_book(exchange = "OKX", symbol = symbol)
+		bids 				= order_book["bids"]
+		average_bid_price 	= self._compute_average_bid_price(bids = bids, size = size)
+		asks 				= order_book["asks"]
+		average_ask_price 	= self._compute_average_ask_price(asks = asks, size = size)
+		return (average_bid_price, average_ask_price)
+
+	def get_perpetual_average_bid_ask_price(self, symbol: str, size: float):
+		"""
+		Returns the average bid / ask price of the perpetual asset, assuming that we intend to trade at a given lot size. 
+		"""
+		order_book 			= self.feed_client.sorted_order_book(exchange = "OKX", symbol = symbol)
+		bids 				= order_book["bids"]
+		average_bid_price 	= self._compute_average_bid_price(bids = bids, size = size)
+		asks 				= order_book["asks"]
+		average_ask_price 	= self._compute_average_ask_price(asks = asks, size = size)
+		return (average_bid_price, average_ask_price)
