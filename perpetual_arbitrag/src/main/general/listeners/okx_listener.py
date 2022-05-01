c029886a3edbd0068fb1fb4dd8ce9595cfd4a34a
johan <JKOK005@e.ntu.edu.sg>
2022-05-02 01:13:41 +0800

    Further developments on okx_listener script
    TODO:
    - Implement polling time for listener script
    - Implement tests on new code development

--- perpetual_arbitrag/src/main/general/listeners/okx_listener.py
+++ perpetual_arbitrag/src/main/general/listeners/okx_listener.py
@@ -1,8 +1,10 @@
 import argparse
+import json
 import logging
 import os
 from clients.OkxApiClient import OkxApiClient
 from db.TradeLogsClient import TradeLogsClient
+from db.SecretsClient import SecretsClient
 
 if __name__ == "__main__":
 	parser 	= argparse.ArgumentParser(description='Log listener')
@@ -11,28 +13,33 @@ if __name__ == "__main__":
 	parser.add_argument('--db_url', type=str, nargs='?', default=os.environ.get("DB_URL"), help="URL pointing to the database")
 	args 	= parser.parse_args()
 	
-	client = OkxApiClient(	
-				api_key = "", 
-				api_secret_key = "", 
-				passphrase = "", 
-				funding_rate_enable = False
-			)
+	secrets_client 		= SecretsClient(db_url = "postgresql://arbitrag_bot:arbitrag@localhost:5432/arbitrag").create_session()
+	trade_logs_client 	= TradeLogsClient(db_url = "postgresql://arbitrag_bot:arbitrag@localhost:5432/arbitrag").create_session()
 
-	recent_transactions = client.get_all_filled_transactions_days(before = 0)
-	db_client = TradeLogsClient(db_url = "postgresql://arbitrag_bot:arbitrag@localhost:5432/arbitrag").create_session()
+	for user_secret_obj in secrets_client.get_all_secrets(exchange = "okx"):
+		api_keys = json.loads(user_secret_obj.api_keys)
+		
+		client = OkxApiClient(	
+					api_key 		= api_keys["API_KEY"], 
+					api_secret_key 	= api_keys["API_SECRET_KEY"], 
+					passphrase 		= api_keys["API_PASSPHRASE"], 
+					funding_rate_enable = False
+				)
 
-	for each_transactions in recent_transactions:
-		db_client.insert(	params = {	
-									"exchange"  	: "okx",
-									"client_id" 	: "123",
-									"page_id" 		: each_transactions["billId"],
-									"order_id"  	: each_transactions["ordId"],
-									"trade_id" 		: each_transactions["tradeId"],
-									"symbol" 		: each_transactions["instId"],
-									"fill_price" 	: each_transactions["fillPx"],
-									"fill_size" 	: each_transactions["fillSz"],
-									"order_side" 	: each_transactions["side"],
-									"pos_side" 		: each_transactions["posSide"],
-									"order_ts" 		: each_transactions["ts"],
-								}
-						)
+		recent_transactions = client.get_all_filled_transactions_days(before = 0)
+
+		for each_transactions in recent_transactions:
+			trade_logs_client.insert(params = {	
+										"exchange"  	: "okx",
+										"client_id" 	: user_secret_obj.client_id,
+										"page_id" 		: each_transactions["billId"],
+										"order_id"  	: each_transactions["ordId"],
+										"trade_id" 		: each_transactions["tradeId"],
+										"symbol" 		: each_transactions["instId"],
+										"fill_price" 	: each_transactions["fillPx"],
+										"fill_size" 	: each_transactions["fillSz"],
+										"order_side" 	: each_transactions["side"],
+										"pos_side" 		: each_transactions["posSide"],
+										"order_ts" 		: each_transactions["ts"],
+									}
+							)
