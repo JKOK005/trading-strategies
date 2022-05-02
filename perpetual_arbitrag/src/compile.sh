d0ed5e2cee32966f1fa4ca74689ddd295e4841a3
johan <JKOK005@e.ntu.edu.sg>
2022-05-02 23:58:21 +0800

    Listener docker file

--- perpetual_arbitrag/src/compile.sh
+++ perpetual_arbitrag/src/compile.sh
@@ -13,3 +13,6 @@ docker build -t jkok005/arb-bot-manager:$version -f ./main/general/intra_exchang
 
 echo "Compiling arbitrag bot scanner"
 docker build -t jkok005/arb-bot-scanner:$version -f ./main/general/scanner/Dockerfile .
+
+echo "Compiling Okx log listener"
+docker build -t jkok005/okx-log-listener:$version -f ./main/general/listeners/Dockerfile .
