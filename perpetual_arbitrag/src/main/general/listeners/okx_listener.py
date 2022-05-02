a50f3b9fb5354dbb5cd7a742b38413d4a79fea92
johan <JKOK005@e.ntu.edu.sg>
2022-05-02 23:17:20 +0800

    Finalize implementation of okx listener script

--- perpetual_arbitrag/src/main/general/listeners/okx_listener.py
+++ perpetual_arbitrag/src/main/general/listeners/okx_listener.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import logging
 import os
+import time
 from clients.OkxApiClient import OkxApiClient
 from db.TradeLogsClient import TradeLogsClient
 from db.SecretsClient import SecretsClient
@@ -11,35 +12,55 @@ if __name__ == "__main__":
 	parser.add_argument('--splits', type=int, nargs='?', default=os.environ.get("SPLITS"), help="Total number of splits on user IDs")
 	parser.add_argument('--split_frac', type=int, nargs='?', default=os.environ.get("SPLIT_FRAC"), help="Fraction of splits that this script handles. Used for sharding and scaling")
 	parser.add_argument('--db_url', type=str, nargs='?', default=os.environ.get("DB_URL"), help="URL pointing to the database")
+	parser.add_argument('--poll_interval_s', type=float, nargs='?', default=os.environ.get("POLL_INTERVAL_S"), help='Poll interval in seconds')
 	args 	= parser.parse_args()
+
+	logging.basicConfig(format='%(asctime)s %(levelname)-8s %(module)s.%(funcName)s %(lineno)d - %(message)s',
+    					level=logging.INFO,
+    					datefmt='%Y-%m-%d %H:%M:%S')
 	
-	secrets_client 		= SecretsClient(db_url = "postgresql://arbitrag_bot:arbitrag@localhost:5432/arbitrag").create_session()
-	trade_logs_client 	= TradeLogsClient(db_url = "postgresql://arbitrag_bot:arbitrag@localhost:5432/arbitrag").create_session()
+	secrets_client 		= SecretsClient(db_url = args.db_url).create_session()
+	trade_logs_client 	= TradeLogsClient(db_url = args.db_url).create_session()
+	page_tracking 		= {}
 
-	for user_secret_obj in secrets_client.get_all_secrets(exchange = "okx"):
-		api_keys = json.loads(user_secret_obj.api_keys)
-		
-		client = OkxApiClient(	
-					api_key 		= api_keys["API_KEY"], 
-					api_secret_key 	= api_keys["API_SECRET_KEY"], 
-					passphrase 		= api_keys["API_PASSPHRASE"], 
-					funding_rate_enable = False
-				)
+	while True:
+		for user_secret_obj in secrets_client.get_all_secrets(exchange = "okx"):
+			if user_secret_obj.ID % args.splits == args.split_frac:
+				try:
+					max_page = page_tracking[user_secret_obj.ID] if user_secret_obj.ID in page_tracking else 0
+					
+					api_keys = json.loads(user_secret_obj.api_keys)
+					
+					client = OkxApiClient(	
+								api_key 		= api_keys["API_KEY"], 
+								api_secret_key 	= api_keys["API_SECRET_KEY"], 
+								passphrase 		= api_keys["API_PASSPHRASE"], 
+								funding_rate_enable = False
+							)	
+					
+					logging.info(f"Fetching transactions for user ID {user_secret_obj.ID} after bill ID {max_page}")
+					recent_transactions = client.get_all_filled_transactions_days(before = max_page)
+					
+					for each_transactions in recent_transactions:
+						trade_logs_client.insert(params = {	
+													"exchange"  	: "okx",
+													"client_id" 	: user_secret_obj.client_id,
+													"page_id" 		: each_transactions["billId"],
+													"order_id"  	: each_transactions["ordId"],
+													"trade_id" 		: each_transactions["tradeId"],
+													"symbol" 		: each_transactions["instId"],
+													"fill_price" 	: each_transactions["fillPx"],
+													"fill_size" 	: each_transactions["fillSz"],
+													"order_side" 	: each_transactions["side"],
+													"pos_side" 		: each_transactions["posSide"],
+													"order_ts" 		: each_transactions["ts"],
+												}
+										)
 
-		recent_transactions = client.get_all_filled_transactions_days(before = 0)
+						max_page = max(int(each_transactions["billId"]), max_page)
+						page_tracking[user_secret_obj.ID] = max_page
 
-		for each_transactions in recent_transactions:
-			trade_logs_client.insert(params = {	
-										"exchange"  	: "okx",
-										"client_id" 	: user_secret_obj.client_id,
-										"page_id" 		: each_transactions["billId"],
-										"order_id"  	: each_transactions["ordId"],
-										"trade_id" 		: each_transactions["tradeId"],
-										"symbol" 		: each_transactions["instId"],
-										"fill_price" 	: each_transactions["fillPx"],
-										"fill_size" 	: each_transactions["fillSz"],
-										"order_side" 	: each_transactions["side"],
-										"pos_side" 		: each_transactions["posSide"],
-										"order_ts" 		: each_transactions["ts"],
-									}
-							)
+				except Exception as ex:
+					logging.error(f"Failed to load user {user_secret_obj} transactions with error: {ex}")
+
+		time.sleep(args.poll_interval_s)
