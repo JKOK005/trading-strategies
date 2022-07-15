6e177c65313f3b19ec18f5ab6bda4b0cb25992c2
johan <JKOK005@e.ntu.edu.sg>
2022-07-15 17:46:57 +0800

    Switched Spot Perpetual bot execution to use idempotent async logic

--- perpetual_arbitrag/src/execution/SpotPerpetualBotExecution.py
+++ perpetual_arbitrag/src/execution/SpotPerpetualBotExecution.py
@@ -25,17 +25,17 @@ class SpotPerpetualBotExecution(BotExecutionV2):
 		self.logger.info(spot_params)
 		self.logger.info(perpetual_params)
 
-		return self.idempotent_trade_execution(	asset_A_order_fn 				= self.api_client.place_perpetual_order,
-												asset_A_params 					= perpetual_params,
-												asset_A_assert_resp_error_fn 	= self.api_client.assert_perpetual_resp_error,
-												asset_A_revert_fn 				= self.api_client.revert_perpetual_order,
-												asset_A_revert_params 			= perpetual_revert_params,
-												asset_B_order_fn 				= self.api_client.place_spot_order,
-												asset_B_params 					= spot_params,
-												asset_B_assert_resp_error_fn 	= self.api_client.assert_spot_resp_error,
-												asset_B_revert_fn 				= self.api_client.revert_spot_order,
-												asset_B_revert_params 			= spot_revert_params
-											)
+		return self.idempotent_trade_execution_async(asset_A_order_fn 				= self.api_client.place_perpetual_order_async,
+													 asset_A_params 				= perpetual_params,
+													 asset_A_assert_resp_error_fn 	= self.api_client.assert_perpetual_resp_error,
+													 asset_A_revert_fn 				= self.api_client.revert_perpetual_order,
+													 asset_A_revert_params 			= perpetual_revert_params,
+													 asset_B_order_fn 				= self.api_client.place_spot_order_async,
+													 asset_B_params 				= spot_params,
+													 asset_B_assert_resp_error_fn 	= self.api_client.assert_spot_resp_error,
+													 asset_B_revert_fn 				= self.api_client.revert_spot_order,
+													 asset_B_revert_params 			= spot_revert_params
+												)
 
 	def short_spot_long_perpetual(self, spot_params,
 									  	perpetual_params,
@@ -52,17 +52,17 @@ class SpotPerpetualBotExecution(BotExecutionV2):
 		self.logger.info(spot_params)
 		self.logger.info(perpetual_params)
 
-		return self.idempotent_trade_execution(	asset_A_order_fn 				= self.api_client.place_spot_order,
-												asset_A_params 					= spot_params,
-												asset_A_assert_resp_error_fn 	= self.api_client.assert_spot_resp_error,
-												asset_A_revert_fn 				= self.api_client.revert_spot_order,
-												asset_A_revert_params 			= spot_revert_params,
-												asset_B_order_fn 				= self.api_client.place_perpetual_order,
-												asset_B_params 					= perpetual_params,
-												asset_B_assert_resp_error_fn 	= self.api_client.assert_perpetual_resp_error,
-												asset_B_revert_fn 				= self.api_client.revert_perpetual_order,
-												asset_B_revert_params 			= perpetual_revert_params
-											)
+		return self.idempotent_trade_execution_async(asset_A_order_fn 				= self.api_client.place_spot_order_async,
+												 	 asset_A_params 				= spot_params,
+												 	 asset_A_assert_resp_error_fn 	= self.api_client.assert_spot_resp_error,
+													 asset_A_revert_fn 				= self.api_client.revert_spot_order,
+												 	 asset_A_revert_params 			= spot_revert_params,
+												 	 asset_B_order_fn 				= self.api_client.place_perpetual_order_async,
+													 asset_B_params 				= perpetual_params,
+												 	 asset_B_assert_resp_error_fn 	= self.api_client.assert_perpetual_resp_error,
+													 asset_B_revert_fn 				= self.api_client.revert_perpetual_order,
+												 	 asset_B_revert_params 			= perpetual_revert_params
+												)
 
 class SpotPerpetualSimulatedBotExecution(SpotPerpetualBotExecution):
 	def __init__(self, *args, **kwargs):
