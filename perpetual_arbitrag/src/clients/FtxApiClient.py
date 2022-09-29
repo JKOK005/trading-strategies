4de592babe53fa7d4bd1d384c1f6dcba5a188d3e
johan <JKOK005@e.ntu.edu.sg>
2022-09-30 00:23:09 +0800

    TODO: Complete implementation of get perpetual funding rate

--- perpetual_arbitrag/src/clients/FtxApiClient.py
+++ perpetual_arbitrag/src/clients/FtxApiClient.py
@@ -24,6 +24,41 @@ class FtxApiClient(ExchangePerpetualClients):
 		self.logger.info(f"Enable for funding rate computation set to {funding_rate_enable}")
 		return
 
+	def _compute_average_margin_purchase_price(self, price_qty_pairs_ordered: [float, float], size: float):
+		"""
+		We will read pricing - qty data from the first entry of the list. 
+
+		This logic will differ, depending on whether we want to go long / short on the asset. 
+		As such, the ordering of the price-qty pairs in the list has to be handled properly by the user. 
+		"""
+		all_trade_qty 		= size
+		trade_amt 			= 0
+		executed_trade_qty 	= 0
+		for each_price_qty_pairs in price_qty_pairs_ordered:
+			if all_trade_qty < 0:
+				break
+			else:
+				[price, qty] 	=  each_price_qty_pairs
+				trade_qty 		=  min(all_trade_qty, qty)
+				trade_amt 	 	+= price * trade_qty
+				all_trade_qty 	-= trade_qty
+				executed_trade_qty += trade_qty
+		return trade_amt / executed_trade_qty
+
+	def _compute_average_bid_price(self, bids: [[float, float]], size: float):
+		# Sell into bids starting from the highest to the lowest.
+		_bids 	= sorted(bids, key = lambda x: x[0], reverse = True)
+		if len(_bids) > 0:
+			return self._compute_average_margin_purchase_price(price_qty_pairs_ordered = _bids, size = size)
+		return 0
+
+	def _compute_average_ask_price(self, asks: [[float, float]], size: float):
+		# Buy into asks starting from the lowest to the highest.
+		_asks 	= sorted(asks, key = lambda x: x[0], reverse = False)
+		if len(_asks) > 0:
+			return self._compute_average_margin_purchase_price(price_qty_pairs_ordered = _asks, size = size)
+		return sys.maxsize
+
 	def get_perpetual_symbols(self):
 		"""
 		Fetches all perpetual instrument symbols
@@ -36,55 +71,97 @@ class FtxApiClient(ExchangePerpetualClients):
 		"""
 		Retrieves perpetual trading account details
 		"""
-		pass
+		resp = self.client.get_positions()
+		relevant_asset_position = next(filter(lambda x: x["future"] == currency, resp))
+		return resp["netSize"]
 
 	def get_perpetual_trading_price(self, symbol: str):
 		"""
 		Retrieves current perpetual price for trading symbol
 		"""
-		pass
+		resp = self.client.get_market(market = symbol)
+		return resp["price"]
 
 	def get_perpetual_min_lot_size(self, symbol: str):
 		"""
 		Retrieves minimum order lot size for perpetual trading symbol
 		"""
-		pass
+		resp = self.client.get_future(future_name = symbol)
+		return resp["sizeIncrement"]
 
 	def get_perpetual_average_bid_ask_price(self, symbol: str, size: float):
 		"""
 		Returns the average bid / ask price of the perpetual asset, assuming that we intend to trade at a given lot size. 
 		"""
-		pass
+		bid_ask_resp 		= client.get_orderbook(market = symbol, depth = 10)
+		
+		bids 				= bid_ask_orders["bids"]
+		average_bid_price 	= self._compute_average_bid_price(bids = bids, size = size)
+
+		asks 				= bid_ask_orders["asks"]
+		average_sell_price 	= self._compute_average_ask_price(asks = asks, size = size)		
+		return (average_bid_price, average_sell_price)
 
 	def get_perpetual_open_orders(self, symbol: str):
 		"""
 		Gets information of all open perpetual orders by the user
 		"""
-		pass
+		open_orders = self.client.get_open_orders(market = symbol)
+		return open_orders
 
 	def get_perpetual_most_recent_open_order(self, symbol: str):
 		"""
 		Gets the most recent open orders for perpetual
 		"""
-		pass
+		open_orders 			= self.get_perpetual_open_orders(market = symbol)
+		sorted_orders 			= sorted(open_orders, key = lambda d: d['createdAt'], reverse = True)
+		most_recent_open_order 	= sorted_orders[0] if len(sorted_orders) > 0 else sorted_orders
+		return most_recent_open_order
 
 	def get_perpetual_fulfilled_orders(self, symbol: str):
 		"""
 		Gets information of all fulfilled perpetual orders by the user
 		"""
-		pass
+		order_history 		= self.client.get_order_history(market = symbol)
+		fulfilled_orders 	= list(filter(lambda x: x["status"] == "closed", order_history))
+		return fulfilled_orders
 
 	def get_perpetual_most_recent_fulfilled_order(self, symbol: str):
 		"""
 		Gets information of the most recent perpetual trade that have been fulfilled
 		"""
-		pass
+		fulfilled_orders 			= self.get_perpetual_fulfilled_orders(symbol = symbol)
+		sorted_orders 				= sorted(fulfilled_orders, key = lambda d: d['createdAt'], reverse = True)
+		most_recent_fulfilled_order = sorted_orders[0] if len(sorted_orders) > 0 else sorted_orders
+		return most_recent_fulfilled_order
 
-	def get_perpetual_effective_funding_rate(self, symbol: str, seconds_before_current: int, seconds_before_estimated: int):
+	def get_perpetual_funding_rate(self, symbol: str):
+		"""
+		Gets the immediate and predicted funding rate for futures contract
+		"""
+		funding_rate_resp = self.public_client.get_funding_rate(instId = symbol)
+		funding_rate_info = funding_rate_resp["data"][0]
+		return (float(funding_rate_info["fundingRate"]), float(funding_rate_info["nextFundingRate"]))
+
+	def funding_rate_valid_interval(self, seconds_before: int):
+		current_time = datetime.datetime.utcnow()
+		for each_snaphsot_time in self.okx_funding_rate_snapshot_times:
+			ts = datetime.datetime.strptime(each_snaphsot_time, "%H:%M")
+			snapshot_timestamp = current_time.replace(hour = ts.hour, minute = ts.minute, second = 0)
+			if 	(snapshot_timestamp - timedelta(seconds = seconds_before) <= current_time and current_time <= snapshot_timestamp) or \
+				(snapshot_timestamp + timedelta(days = 1) - timedelta(seconds = seconds_before) <= current_time and current_time <= snapshot_timestamp + timedelta(days = 1)):
+				return True
+		return False
+
+	def get_perpetual_effective_funding_rate(self, symbol: str, seconds_before_current: int):
 		"""
 		Gets the effective funding rate for perpetual contract.
 
 		Effective funding rate takes into account a variety of factors to decide on the funding rate.
+
+		1) If we are not within a valid funding interval, then the funding rates are 0.
+		2) If we are within the valid funding interval, then the funding rate is non-zero.
+		3) If funding rate computation has been disabled, then all rates are 0.
 		"""
 		pass
 
